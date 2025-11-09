5531e21012bcbebbc045e17c82480bf84130d28f
Cristan Medina <crishighlights@gmail.com>
2025-11-09 11:30:32 -0800

    Creacion de paginas, pagina mapa ya muestra mapa, creacion de componente bottom tab bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { BottomTabBarComponent } from "./components/bottom-tab-bar/bottom-tab-bar.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, BottomTabBarComponent],
 })
 export class AppComponent {
   constructor() {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,41 @@
 import { Routes } from '@angular/router';
+import { BottomTabBarComponent } from './components/bottom-tab-bar/bottom-tab-bar.component';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    component: BottomTabBarComponent,
+    children: [
+        {
+        path: 'signup',
+        loadComponent: () => import('./pages/signup/signup.page').then( m => m.SignupPage)
+        },
+        {
+            path: 'login',
+            loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+        },
+        {
+            path: 'profile',
+            loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage)
+        },
+        {
+            path: 'events',
+            loadComponent: () => import('./pages/events/events.page').then( m => m.EventsPage)
+        },
+        {
+            path: 'map',
+            loadComponent: () => import('./pages/map/map.page').then( m => m.MapPage),
+        },
+        {
+            path: '',
+            redirectTo: 'map',
+            pathMatch: 'full'
+        }
+    ]
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+    redirectTo: '/map',
+    pathMatch: 'full'
+  }
 ];
